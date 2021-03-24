e6cb69f36de4b67397b53ec0a07bab562fab5934
suwijak0023 <70879387+suwijak0023@users.noreply.github.com>
2021-03-24 21:30:31 +0700

    HW

---
+++ 2.py
@@ -0,0 +1,10 @@
+def volume_c():
+    r = 2
+    pi = 3.14
+    h = 4
+    sum = pi*(r**2)*h
+    print(sum)
+
+volume_c()
+
+
---
+++ 3.py
@@ -0,0 +1,8 @@
+def volume_s():
+    pi = 3.14
+    r = 2
+    sum = pi*(r**3)*(4/3)
+    print(sum);
+
+volume_s()
+
